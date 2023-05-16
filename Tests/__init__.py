d97e1acb83941a23fdf167b4c2addd707f9c53bd
riccardozulla <riccardozulla@gmail.com>
2023-05-16 17:51:55 +0200

    update __init__.py

--- Tests/__init__.py
+++ Tests/__init__.py
@@ -1,7 +1,6 @@
-# from Tests import TestCommands, TestStuffOnStreets
 from Tests import TestCommands
 from Tests import TestStuffOnStreets
 from Tests import TestStuffOnVictoryPoints
 from Tests import TestStuffOnKnights
 from Tests import TestStuffOnMonopoly
-from Tests import testStuffOnRoadBuilding
+from Tests import TestStuffOnRoadBuilding
